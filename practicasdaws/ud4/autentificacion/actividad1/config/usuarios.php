6956e57ac347303e9116a5752beda679701d0e7e
Sergio Luna Martin <sergiolunamr@gmail.com>
2023-10-23 10:02:50 +0200

    23/10/2023

---
+++ practicasdaws/ud4/autentificacion/actividad1/config/usuarios.php
@@ -0,0 +1,9 @@
+<?php
+
+$usuarios= array(
+    array("user" => "us1", "password" => "us1"),
+    array("user" => "us2", "password" => "us2"),
+)
+
+//Acceder a us1 = usuarios[0][user]
+?>
---
+++ practicasdaws/ud4/autentificacion/actividad1/privado2.php
@@ -0,0 +1,53 @@
+<?php
+/*
+author: Sergio Luna Martín
+date: 20/10/2023
+*/
+include("config/usuarios.php");
+
+//Iniciamos sesión
+session_start();
+if(!$_SESSION["auth"]) {
+    header("location:inicio.php");
+}
+$auth = $_SESSION["auth"];
+$user = $_SESSION["user"];
+
+?>
+<!DOCTYPE html>
+<html lang='es'>
+
+<head>
+    <meta charset='UTF-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <meta name='author' content='SergioLunaMartín'>
+    <script src='main.js'></script>
+    <title>SergioLunaMartín</title>
+</head>
+
+<body>
+    <h1>Autentificación básica</h1>
+    <div>
+        <?php
+        if ($auth) {
+            echo "<p>Bievenido. ${user}. <a href='closesession.php'>Salir</a></p>";
+        } else {
+            include("include/login_form.php");
+        }
+        ?>
+    </div>
+    <nav>
+        <?php
+        if ($auth) {
+            include("include/nav_usuario.php");
+        } else {
+            include("include/nav_invitado.php");
+        }
+        ?>
+    </nav>
+    <h2>Privado 2</h2>
+
+</body>
+
+</html>
