bb9fb4854dc62d1de8ace108661ad75a886321bd
Sergio Luna Martín <sergiolunamarti@gmail.com>
2023-11-08 00:17:45 +0100

    c

--- practicasdiweb/ud2/practica4canvas1/main.js
+++ practicasdiweb/ud2/practica4canvas1/main.js
@@ -1,5 +1,4 @@
 // Comprobar que encontramos un elemento y podemos extraer su contexto.
-
 let state = 1;
 
 function cargaContextoCanvas(idCanvas) {
@@ -13,30 +12,31 @@ function cargaContextoCanvas(idCanvas) {
     return false;
 }
 
+let canvas = document.getElementById("miCanvas");
+let contexto = cargaContextoCanvas("miCanvas");
+let x = 50;  // Posición inicial del rectángulo
+let velocidad = 2; // Velocidad de desplazamiento
+
+// Función para dibujar el rectángulo
+function dibujarRectangulo() {
+    contexto.clearRect(0, 0, canvas.width, canvas.height);
+
+    contexto.fillStyle = "blue";
+    contexto.fillRect(x, 50, 50, 30);
+
+    x += velocidad;
+
+    if (x + 50 > canvas.width || x < 0) {
+        velocidad = -velocidad; // Cambia la dirección cuando llega a los bordes
+    }
+}
+
+// Función de animación utilizando setInterval
+function animar() {
+    dibujarRectangulo();
+}
+
+// Iniciar la animación en el evento DOMContentLoaded
 window.addEventListener("DOMContentLoaded", function () {
-    var canvas = document.getElementById("miCanvas");
-    var x = 50;  // Posición inicial del rectángulo
-    var velocidad = 2; // Velocidad de desplazamiento
-    
-    // Función para dibujar el rectángulo
-    function dibujarRectangulo() {
-        contexto.clearRect(0, 0, canvas.width, canvas.height);
-
-        contexto.fillStyle = "blue";
-        contexto.fillRect(x, 50, 50, 30);
-
-        x += velocidad;
-    
-        if (x + 50 > canvas.width || x < 0) {
-            velocidad = -velocidad; // Cambia la dirección cuando llega a los bordes
-        }
-    }
-    
-    // Función de animación utilizando setInterval
-    function animar() {
-        setInterval(dibujarRectangulo, 16); // 16 ms para una animación a 60 fps
-    }
-    
-    // Iniciar la animación
-    animar();
+    setInterval(animar, 16); // 16 ms para una animación a 60 fps
 });
