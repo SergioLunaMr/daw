04bdcc7cf910b94293a918bebea8bcbda0b0fe9b
Sergio Luna Martin <sergiolunamr@gmail.com>
2023-12-12 12:51:07 +0100

    autoescuela casi funcional

--- practicasdaws/RA3/RA3 IT2/autoescuela/index.php
+++ practicasdaws/RA3/RA3 IT2/autoescuela/index.php
@@ -3,43 +3,55 @@
 
 // Incluye el archivo de tests
 include 'config/test.php';
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+$stringaux;
+if (isset($_POST['iniciar'])) {
     // Procesamiento del formulario
     $testId = $_POST['test'];
-    if(isset($_POST['respuesta'])){
-        $respuestasUsuario = $_POST['respuesta'];
+    // Muestra el formulario con las preguntas del test seleccionado
+    echo "<form method='post' action='index.php'>";
+    foreach ($aTests[$testId - 1]['Preguntas'] as $pregunta) {
+        $stringaux = "<p>{$pregunta['Pregunta']}</p>";
+        $stringaux = $stringaux . `<label for='{$pregunta['idPregunta']}'>Tu respuesta:</label>`;
+        $stringaux = $stringaux . "<select name='{$pregunta['idPregunta']}' id='{$pregunta['idPregunta']}'>";
+        foreach ($pregunta['respuestas'] as $respuesta) {;
+            $stringaux = $stringaux . "<option value='" . substr($respuesta, 0, 1) . "'>" . $respuesta. "</option>";
+        }
+        $stringaux = $stringaux . "</select>";
+        echo $stringaux;
+
     }
+    echo "<input type='submit' value='Finalizar Test'>";
+    echo "<input type='hidden' name='test' value='$testId'>";
+    echo "</form>";
+    // // Obtén las preguntas y respuestas correctas del test seleccionado
+    // $test = $aTests[$testId - 1];
+    // $respuestasCorrectas = $test['Corrector'];
 
-    // Obtén las preguntas y respuestas correctas del test seleccionado
-    $test = $aTests[$testId - 1];
-    $respuestasCorrectas = $test['Corrector'];
+    // // Compara las respuestas del usuario con las respuestas correctas
+    // $respuestasCorrectasUsuario = array_map(function ($respuestaUsuario, $respuestaCorrecta) {
+    //     return $respuestaUsuario == $respuestaCorrecta;
+    // }, $respuestasUsuario, $respuestasCorrectas);
 
-    // Compara las respuestas del usuario con las respuestas correctas
-    $respuestasCorrectasUsuario = array_map(function ($respuestaUsuario, $respuestaCorrecta) {
-        return $respuestaUsuario == $respuestaCorrecta;
-    }, $respuestasUsuario, $respuestasCorrectas);
+    // // Determina si el test fue superado
+    // $testSuperado = !in_array(false, $respuestasCorrectasUsuario);
 
-    // Determina si el test fue superado
-    $testSuperado = !in_array(false, $respuestasCorrectasUsuario);
+    // // Muestra el resultado utilizando emoticonos
+    // $resultado = ($testSuperado) ? '🎉 SUPERADO' : '😞 NO SUPERADO';
 
-    // Muestra el resultado utilizando emoticonos
-    $resultado = ($testSuperado) ? '🎉 SUPERADO' : '😞 NO SUPERADO';
+    // echo "<p>Resultado del test: $resultado</p>";
 
-    echo "<p>Resultado del test: $resultado</p>";
+    // // Muestra las respuestas correctas e incorrectas
+    // echo "<p>Respuestas correctas: ";
+    // foreach ($respuestasCorrectasUsuario as $idPregunta => $esCorrecta) {
+    //     echo ($esCorrecta) ? "$idPregunta " : "";
+    // }
+    // echo "</p>";
 
-    // Muestra las respuestas correctas e incorrectas
-    echo "<p>Respuestas correctas: ";
-    foreach ($respuestasCorrectasUsuario as $idPregunta => $esCorrecta) {
-        echo ($esCorrecta) ? "$idPregunta " : "";
-    }
-    echo "</p>";
-
-    echo "<p>Respuestas incorrectas: ";
-    foreach ($respuestasCorrectasUsuario as $idPregunta => $esCorrecta) {
-        echo (!$esCorrecta) ? "$idPregunta " : "";
-    }
-    echo "</p>";
+    // echo "<p>Respuestas incorrectas: ";
+    // foreach ($respuestasCorrectasUsuario as $idPregunta => $esCorrecta) {
+    //     echo (!$esCorrecta) ? "$idPregunta " : "";
+    // }
+    // echo "</p>";
 } else {
     // Muestra el formulario de selección de tests
     echo "<form method='post' action='index.php'>";
@@ -49,20 +61,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         echo "<option value='{$test['idTest']}'>Test {$test['idTest']} - {$test['Permiso']} - {$test['Categoria']}</option>";
     }
     echo "</select>";
-    echo "<input type='submit' value='Iniciar Test'>";
+    echo "<input type='submit' name='iniciar' value='Iniciar Test'>";
     echo "</form>";
 }
 
 if (isset($testId)) {
-    // Muestra el formulario con las preguntas del test seleccionado
-    echo "<form method='post' action='index.php'>";
-    foreach ($aTests[$testId - 1]['Preguntas'] as $pregunta) {
-        echo "<p>{$pregunta['Pregunta']}</p>";
-        echo "<label for='respuestas[{$pregunta['idPregunta']}]'>Tu respuesta:</label>";
-        echo "<input type='text' name='respuestas[{$pregunta['idPregunta']}]'>";
-    }
-    echo "<input type='submit' value='Finalizar Test'>";
-    echo "<input type='hidden' name='test' value='$testId'>";
-    echo "</form>";
+
 }
 ?>
